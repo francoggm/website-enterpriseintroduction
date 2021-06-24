af2fd00daaba29e168927bc7b6090a60f049a9df
Gabriel Franco <82771295+francoggm@users.noreply.github.com>
2021-06-24 14:05:49 -0300

    Add files via upload

---
+++ projeto fruta e frutos - figma alura/assets/js/index.js
@@ -0,0 +1,10 @@
+$('.banner__carrosel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+
+});
